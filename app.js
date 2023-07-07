69bcd8ec4a132603ab3c1f2c8785ab7b009d37ef
faiz <faizkamran001@gmail.com>
2023-07-07 17:17:03 +0530

    folders added

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+// Define your API routes
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
